bf432b17cb6b2faf3f39868adce592d99b2157ef
kriba24 <75278579+kriba24@users.noreply.github.com>
2022-05-06 09:16:25 -0400

    update python project 5/6

--- Python Final Project/Final Project.py
+++ Python Final Project/Final Project.py
@@ -3,14 +3,37 @@ def random_nums():
     num1 = random.randint(0,100)
     num2 = random.randint(0,100)
     return num1, num2
-
 print( """
 Enter the sign for the problem type desired.
     + Addition
     - Subtraction
     * Multiplication
 """ )
-sign = input('Choice: ')
-sign = sign.lower()
-sign = sign.strip()
-print(sign)
+num1, num2 = random_nums()
+print(num1, num2)
+score = 0
+while True:
+    sign = input('Choice: ')
+    if sign == "+":
+        correct_answer = num1 + num2
+        break
+    elif sign == "-":
+        correct_answer = num1 - num2
+        break
+    elif sign == '*':
+        correct_answer = num1 * num2
+        break
+    else:
+        print('Try again!')
+        continue
+
+def get_guess():
+    user_answer = int(input(str(num1) + ' ' + sign + ' ' + str(num2) + " = "))
+    return user_answer 
+
+for i in range(10):
+    user_answer = get_guess()
+    if user_answer == correct_answer:
+        print('Correct!')
+        score += 10
+    random_nums()
